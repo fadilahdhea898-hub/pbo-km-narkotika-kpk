3e896760297e27645753b2af96a6573c31dc63ac
bishamon315 <muhrizkyadri@gmail.com>
2026-06-24 20:28:34 +0700

    feat: add Putusan private fields

--- src/model/Putusan.java
+++ src/model/Putusan.java
@@ -1,6 +1,19 @@
 package model;
 
 public class Putusan extends DokumenHukum implements Laporan {
+    private String pengadilan;
+    private String tanggalPutusan;
+    private String namaTerdakwa;
+    private int umurTerdakwa;
+    private String jenisNarkotika;
+    private double beratBarangBukti;
+    private String pasalDilanggar;
+    private String peranTerdakwa;
+    private int vonisHukuman;
+    private double vonisDenda;
+    private String namaHakim;
+
+    private static int jumlahDibuat = 0;
 
     @Override
     public void tampilkan() {
