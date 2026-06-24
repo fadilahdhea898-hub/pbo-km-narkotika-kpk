602be68cd6f77148b20287b28e6fa3389bd7d613
azizsaputra <abdulazizsaputra40@webmail.umm.ac.id>
2026-06-24 20:44:19 +0700

    feat: add detail and message display

--- src/view/ConsoleView.java
+++ src/view/ConsoleView.java
@@ -78,4 +78,16 @@ public class ConsoleView {
             p.tampilkan();
         }
     }
+    public void tampilkanDetail(Putusan p) {
+        if (p == null) {
+            System.out.println("Data tidak ditemukan.");
+        } else {
+            System.out.println("\n===== DETAIL PUTUSAN =====");
+            System.out.println(p);
+        }
+    }
+
+    public void tampilkanPesan(String pesan) {
+        System.out.println(pesan);
+    }
 }
